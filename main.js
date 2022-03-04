7a956caa027112ac4e55bb4a176855ae517b9c80
Caio Severino <kaiosev9@gmail.com>
2022-03-04 00:51:42 -0300

    product finalizado

--- main.js
+++ main.js
@@ -13,3 +13,14 @@ if(navClose){
         navMenu.classList.remove('show-menu')
     })
 }
+
+function scrollHeader(){
+    const header = document.getElementById('header')
+
+    if(this.scrollY >= 80){
+        header.classList.add('scroll-header')
+    }else{
+        header.classList.remove('scroll-header')
+    }
+}
+window.addEventListener('scroll', scrollHeader)
